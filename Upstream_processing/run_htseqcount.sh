e0b898ee70e5b0ebdd748048ba1b37d6337ce8bf
mcadamme <mcadamme@msu.edu>
2021-02-16 12:00:08 -0500

    made what my workflow and rational (for sanity check in next script) clearer

--- Upstream_processing/run_htseqcount.sh
+++ Upstream_processing/run_htseqcount.sh
@@ -27,19 +27,8 @@ htseq-count -f bam -a 20 -r pos -s reverse -t exon  M4-2_S14.bam /media/fritzlab
 htseq-count -f bam -a 20 -r pos -s reverse -t exon  M4-3_S15.bam /media/fritzlab/EE9C16C89C168AEB/Noreuil/trimmed_pairs/mosquito_genome_assemblies/Culex-quinquefasciatus-Johannesburg_BASEFEATURES_CpipJ2.4.gtf > ./highQual_exon/M4-3_S15_htseq
 htseq-count -f bam -a 20 -r pos -s reverse -t exon  M4-4_S16.bam /media/fritzlab/EE9C16C89C168AEB/Noreuil/trimmed_pairs/mosquito_genome_assemblies/Culex-quinquefasciatus-Johannesburg_BASEFEATURES_CpipJ2.4.gtf > ./highQual_exon/M4-4_S16_htseq
 
-#highQual_gene
-htseq-count -f bam -a 20 -r pos -s reverse --idattr=gene_id M1-1_S1.bam  /media/fritzlab/EE9C16C89C168AEB/Noreuil/trimmed_pairs/mosquito_genome_assemblies/Culex-quinquefasciatus-Johannesburg_BASEFEATURES_CpipJ2.4.gtf > ./highQual_gene/M1-1_S1_htseq
-htseq-count -f bam -a 20 -r pos -s reverse --idattr=gene_id  M1-2_S2.bam  /media/fritzlab/EE9C16C89C168AEB/Noreuil/trimmed_pairs/mosquito_genome_assemblies/Culex-quinquefasciatus-Johannesburg_BASEFEATURES_CpipJ2.4.gtf > ./highQual_gene/M1-2_S2_htseq
-htseq-count -f bam -a 20 -r pos -s reverse --idattr=gene_id  M1-3_S3.bam  /media/fritzlab/EE9C16C89C168AEB/Noreuil/trimmed_pairs/mosquito_genome_assemblies/Culex-quinquefasciatus-Johannesburg_BASEFEATURES_CpipJ2.4.gtf > ./highQual_gene/M1-3_S3_htseq
-htseq-count -f bam -a 20 -r pos -s reverse --idattr=gene_id  M1-4_S4.bam  /media/fritzlab/EE9C16C89C168AEB/Noreuil/trimmed_pairs/mosquito_genome_assemblies/Culex-quinquefasciatus-Johannesburg_BASEFEATURES_CpipJ2.4.gtf > ./highQual_gene/M1-4_S4_htseq
+#highQual_gene example - wasn't sure about the function of --idattr so I tested it.  Here is an example command.
 htseq-count -f bam -a 20 -r pos -s reverse --idattr=gene_id  M2-1_S5.bam  /media/fritzlab/EE9C16C89C168AEB/Noreuil/trimmed_pairs/mosquito_genome_assemblies/Culex-quinquefasciatus-Johannesburg_BASEFEATURES_CpipJ2.4.gtf > ./highQual_gene/M2-1_S5_htseq
-htseq-count -f bam -a 20 -r pos -s reverse --idattr=gene_id  M2-2_S6.bam  /media/fritzlab/EE9C16C89C168AEB/Noreuil/trimmed_pairs/mosquito_genome_assemblies/Culex-quinquefasciatus-Johannesburg_BASEFEATURES_CpipJ2.4.gtf > ./highQual_gene/M2-2_S6_htseq
-htseq-count -f bam -a 20 -r pos -s reverse --idattr=gene_id  M2-3_S7.bam  /media/fritzlab/EE9C16C89C168AEB/Noreuil/trimmed_pairs/mosquito_genome_assemblies/Culex-quinquefasciatus-Johannesburg_BASEFEATURES_CpipJ2.4.gtf > ./highQual_gene/M2-3_S7_htseq
-htseq-count -f bam -a 20 -r pos -s reverse --idattr=gene_id  M2-4_S8.bam  /media/fritzlab/EE9C16C89C168AEB/Noreuil/trimmed_pairs/mosquito_genome_assemblies/Culex-quinquefasciatus-Johannesburg_BASEFEATURES_CpipJ2.4.gtf > ./highQual_gene/M2-4_S8_htseq
-htseq-count -f bam -a 20 -r pos -s reverse --idattr=gene_id  M4-1_S13.bam /media/fritzlab/EE9C16C89C168AEB/Noreuil/trimmed_pairs/mosquito_genome_assemblies/Culex-quinquefasciatus-Johannesburg_BASEFEATURES_CpipJ2.4.gtf > ./highQual_gene/M4-1_S13_htseq
-htseq-count -f bam -a 20 -r pos -s reverse --idattr=gene_id  M4-2_S14.bam /media/fritzlab/EE9C16C89C168AEB/Noreuil/trimmed_pairs/mosquito_genome_assemblies/Culex-quinquefasciatus-Johannesburg_BASEFEATURES_CpipJ2.4.gtf > ./highQual_gene/M4-2_S14_htseq
-htseq-count -f bam -a 20 -r pos -s reverse --idattr=gene_id  M4-3_S15.bam /media/fritzlab/EE9C16C89C168AEB/Noreuil/trimmed_pairs/mosquito_genome_assemblies/Culex-quinquefasciatus-Johannesburg_BASEFEATURES_CpipJ2.4.gtf > ./highQual_gene/M4-3_S15_htseq
-htseq-count -f bam -a 20 -r pos -s reverse --idattr=gene_id  M4-4_S16.bam /media/fritzlab/EE9C16C89C168AEB/Noreuil/trimmed_pairs/mosquito_genome_assemblies/Culex-quinquefasciatus-Johannesburg_BASEFEATURES_CpipJ2.4.gtf > ./highQual_gene/M4-4_S16_htseq
 
 #DiffStrandedness_gene - quick check.
 htseq-count -f bam -a 20 -r pos -s yes --idattr=gene_id  M2-1_S5.bam  /media/fritzlab/EE9C16C89C168AEB/Noreuil/trimmed_pairs/mosquito_genome_assemblies/Culex-quinquefasciatus-Johannesburg_BASEFEATURES_CpipJ2.4.gtf > ./Str_Yes/M2-1_S5_htseq
